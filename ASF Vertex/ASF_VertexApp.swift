dcfaa41639000a07c4697dc62d6fd44bcd83e976
Andrew Anderson <raanderson9@alaska.edu>
2023-01-19 13:55:29 -0800

    Initial Commit

---
+++ ASF Vertex/ASF_VertexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASF_VertexApp.swift
+//  ASF Vertex
+//
+//  Created by Andrew Anderson on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ASF_VertexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASF VertexUITests/ASF_VertexUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ASF_VertexUITestsLaunchTests.swift
+//  ASF VertexUITests
+//
+//  Created by Andrew Anderson on 1/19/23.
+//
+
+import XCTest
+
+final class ASF_VertexUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
