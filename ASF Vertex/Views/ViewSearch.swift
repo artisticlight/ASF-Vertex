1e61aca00a9889872224c3031c04c99ab542605c
Andrew Anderson <andrew@artisticlight.com>
2023-01-23 16:00:23 -0800

    Navigation Stack working

--- ASF Vertex/Views/ViewSearch.swift
+++ ASF Vertex/Views/ViewSearch.swift
@@ -9,23 +9,42 @@ import MapKit
 import SwiftUI
 
 struct ViewSearch: View {
+    @State public var SearchType: [String] =
+        ["Geographic",
+        "List",
+        "Event"]
+        
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-    
+
     var body: some View {
-        Form {
-            Section {
-                Text("Event Search")
-            }
-            Section {
-                ZStack {
-                    Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+        let numOfTypes: Int = SearchType.count
+        ZStack {
+            Color.blue
+            NavigationStack {
+                Form {
+                    Section {
+                        List(0..<numOfTypes, id:\.self) { i in
+                            NavigationLink {
+                                Text("Hello (searchType)")
+                            } label: {
+                                Label("hello \(SearchType[i])", systemImage:"circle")
+                            }
+                        }
+                    }
+                    Section {
+                            Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+                    }
+                    .frame(width: .infinity, height: 500)
                 }
+                .navigationTitle("Search")
+                .navigationBarTitleDisplayMode(.large)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.blue)
             }
-            .frame(width: .infinity, height: 500)
+
         }
     }
 }
-
 
 struct ViewSearch_Previews: PreviewProvider {
     static var previews: some View {
